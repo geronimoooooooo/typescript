21f5305a03715a25ebfedaab6cda0d9e752d26f6
geronimoooooooo <geronimoooooooo@gmail.com>
2019-07-21 11:41:34 +0200

    Update for_loops.ts

--- for_loops.ts
+++ for_loops.ts
@@ -18,3 +18,36 @@ for (const i of list) {
 for (const i in list) {
    console.log(i); // "0", "1", "2",
 }
+-----------------------------------------
+The difference between for…of and for…in
+https://imasters.com/development/the-difference-between-for-of-and-for-in/?trace=1519021197&source=single
+  
+const numbers = [1,2,3,4,5];
+ for(let numbers of numbers) {
+  console.log(number);
+ }
+
+// result: 1, 2, 3, 4, 5
+
+const numbers = [1,2,3,4,5];
+for(let number in number) {
+ console.log(number);
+}
+
+// result: 0, 1, 2, 3, 4
+  
+  const House = {
+ area: 1000,
+ height: 7,
+ floors: 2
+}
+
+for(let prop in House) {
+ console.log(prop);
+}
+
+// Result
+// area
+// height
+// floors
+------------------------------
