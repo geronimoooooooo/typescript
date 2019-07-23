7b2b2bde73f1812ccd4ae711aa96af3a2ae91980
geronimoooooooo <geronimoooooooo@gmail.com>
2019-07-23 17:56:56 +0200

    Update AAA_tldr.ts

--- AAA_tldr.ts
+++ AAA_tldr.ts
@@ -2,11 +2,11 @@
 https://www.typescriptlang.org/docs/handbook/basic-types.html#type-assertions
 <br>Type assertions are a way to tell the compiler “trust me, I know what I’m doing.” A type assertion is like a type cast in other languages.
 
-```typescript
+
 let someValue: any = "this is a string";
 let strLength: number = (<string>someValue).length;
 let strLength: number = (someValue as string).length;
-```
+
 ###############################################################
 Typescript has a concept of optional attributes/functions. These are denoted with a ? at the end like so:
 interface MyInterface 
@@ -16,10 +16,8 @@ interface MyInterface
     optionalFunction?: Function;
 }
 ####################################################################################
-```typescript
 var num = 12 
 console.log(typeof num);   //output: number
-```
 ####################################################################################
 Fields which do not require initialisation
 public someUselessValue!: number;
