08825da9c1d399d2b54923e54db4bec1f8010a62
geronimoooooooo <geronimoooooooo@gmail.com>
2019-07-26 00:40:29 +0200

    Create decorator.ts

---
+++ decorator.ts
@@ -0,0 +1,3 @@
+https://www.logicbig.com/tutorials/misc/typescript/class-decorators.html
+https://www.typescriptlang.org/docs/handbook/decorators.html
+https://codeburst.io/decorate-your-code-with-typescript-decorators-5be4a4ffecb4
