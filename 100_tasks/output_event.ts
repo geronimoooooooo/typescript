37b78a724e1cfdc35980613b6e3e758abb4960b2
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-01 22:37:35 +0200

    Create output_event.ts

---
+++ 100_tasks/output_event.ts
@@ -0,0 +1,14 @@
+<joke-form 
+  (jokeCreated)="addJoke($event)">
+</joke-form>
+
+// output event property on the component.
+class JokeFormComponent {
+  @Output() jokeCreated = new EventEmitter<Joke>();
+  
+  createJoke() {
+    this.jokeCreated.emit(new Joke("A setup", "A punchline"));
+  }
+}
+The name between the <> on the EventEmitter is the type of thing that will be
+output by this property.
