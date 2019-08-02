ab786b3ae903f883bb12a3e6acbb961f17ab1a67
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-02 14:43:40 +0200

    Update and rename deconstructering_spread.ts to destructering_spread.ts

--- destructering_spread.ts
+++ destructering_spread.ts
@@ -23,3 +23,20 @@ This gives bothPlus the value [0, 1, 2, 3, 4, 5]. Spreading creates a shallow co
 //object
 let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
 let search = { ...defaults, food: "rich" };
+###############################
+
+Destructuring is a way of extracting values into variables from data stored in objects and arrays.
+const obj = {first: 'Asim', last: 'Hussain', age: 39 };
+const {first: f, last: l} = obj;
+console.log(f); // Asim
+console.log(l); // Hussain
+
+const {first: f} = obj; translates to extract the property first and store in aconstant called f.
+const {first, last} = obj;
+
+
+Array destructuring works in a similar way except it extracts based of the index in the array, like so:
+const arr = ['a', 'b'];
+const [x, y] = arr;
+console.log(x); // a
+console.log(y); // b
