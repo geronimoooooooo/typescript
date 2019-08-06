5428f54b8fee2c5cac54f4ce49c0523b54f9d0be
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-06 19:56:03 +0200

    Create commands.ts

---
+++ commands.ts
@@ -0,0 +1,5 @@
+
+npm install -g angular-cli          Install the CLI
+ng -v, --version                    Check the Angular version    
+ng new project                      Creates a new project
+ng serve                            Serve our application using a local web-server
