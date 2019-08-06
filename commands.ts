9c54c0cd89e8ce52b85d40f58937326f656aeb9f
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-06 22:57:07 +0200

    Update commands.ts

--- commands.ts
+++ commands.ts
@@ -3,3 +3,8 @@ npm install -g angular-cli          Install the CLI
 ng -v, --version                    Check the Angular version    
 ng new project                      Creates a new project
 ng serve                            Serve our application using a local web-server
+ng generate component <CompName>    Create a component
+(ng g c <CompName>)
+ng g component ./src/app/foo/bar    Create a component called BarComponent in the folder ./src/app/foo/bar
+ng g service MyService              Creates MyService
+ng g class MyClass                  Creates MyClass
