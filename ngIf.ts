24a7c8105e0cfa04726f63b3230768a57acd3454
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-20 15:41:08 +0200

    Create ngIf.ts

---
+++ ngIf.ts
@@ -0,0 +1,8 @@
+*ngIf="<condition>"
+
+//The NgIf directive removes the li element from the DOM if person.age is less than 30.
+<ul *ngFor="let person of people">
+<li *ngIf="person.age < 30">
+{{ person.name }} ({{ person.age }})
+</li>
+</ul>
