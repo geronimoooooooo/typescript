2b332d1471655f709ec37fff466a21ed70483900
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-25 00:48:41 +0200

    Update binding.ts

--- binding.ts
+++ binding.ts
@@ -2,5 +2,11 @@
 
 
 <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+#######################################  
+<app-hero-detail [hero]="selectedHero"></app-hero-detail>
+[hero]="selectedHero" is an Angular property binding.
 
+It's a one way data binding from the selectedHero property of the HeroesComponent to the hero property of the target element, which maps to the hero property of the HeroDetailComponent.
+
+Now when the user clicks a hero in the list, the selectedHero changes. When the selectedHero changes, the property binding updates hero and the HeroDetailComponent displays the new hero.
 ###########################################
