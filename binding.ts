169ea050f01c5693ec0cab507ef3081bc12bde62
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-25 01:31:45 +0200

    Update binding.ts

--- binding.ts
+++ binding.ts
@@ -5,7 +5,9 @@
 <app-hero-detail [hero]="selectedHero"></app-hero-detail>
 [hero]="selectedHero" is an Angular property binding.
 
-It's a one way data binding from the selectedHero property of the HeroesComponent to the hero property of the target element, which maps to the hero property of the HeroDetailComponent.
+It's a one way data binding from the selectedHero property of the HeroesComponent to the hero property of the target element, 
+which maps to the hero property of the HeroDetailComponent.
 
-Now when the user clicks a hero in the list, the selectedHero changes. When the selectedHero changes, the property binding updates hero and the HeroDetailComponent displays the new hero.
+Now when the user clicks a hero in the list, the selectedHero changes. When the selectedHero changes, the property binding updates hero 
+and the HeroDetailComponent displays the new hero.
 ###########################################
