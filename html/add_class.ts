e22a2c6e0c20bb9e512c588a8cb10756602a7eb5
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-24 23:50:33 +0200

    Create add_class.ts

---
+++ html/add_class.ts
@@ -0,0 +1,3 @@
+The Angular class binding makes it easy to add and remove a CSS class conditionally. Just add [class.some-css-class]="some-condition" to the element you want to style.
+
+[class.selected]="hero === selectedHero"
