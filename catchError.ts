d5d99a7a1399cc5be0ac9104e879470fdc152f51
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-26 12:12:57 +0200

    Create catchError.ts

---
+++ catchError.ts
@@ -0,0 +1,7 @@
+getHeroes (): Observable<Hero[]> {
+  return this.http.get<Hero[]>(this.heroesUrl)
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+}
+#########################################################
