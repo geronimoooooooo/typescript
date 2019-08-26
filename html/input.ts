3964a43f9a6744f05680fccb5aa150aeef1c525f
geronimoooooooo <geronimoooooooo@gmail.com>
2019-08-26 12:47:07 +0200

    Update input.ts

--- html/input.ts
+++ html/input.ts
@@ -3,3 +3,6 @@
     <input [(ngModel)]="hero.name" placeholder="name"/>
   </label>
 </div>
+#####################################################
+  <input #searchBox id="search-box" (input)="search(searchBox.value)" />
+#####################################################
